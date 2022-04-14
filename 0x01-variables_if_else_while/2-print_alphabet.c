4b44e4de4eaf3cf49dae8463eb06cf5ec42ed513
Getabalew7 <60742895+Getabalew7@users.noreply.github.com>
2022-04-14 12:48:49 +0300

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,22 +1,15 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
- *
  * Return: Always 0 (Success)
  */
 int main(void)
-{int num;
-num = 97;
-while (num <= 122)
-putchar(num);
-num++;
-int p;
-p = 97;
-while (p <= 122)
 {
-putchar(p);
-p++;
-}
-putchar('\n');
-return (0);
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
 }
